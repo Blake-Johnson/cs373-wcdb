45ef104acb0b1849ae835e984738a859122b302f
stephenc <stephenchiang@utexas.edu>
2013-07-06 08:18:00 -0700

    refactored project structure

--- crisis/crisis/static/crisis_app/js/custom.js
+++
@@ -1,9 +0,0 @@
-function link(selector){
-  if(document.body.style['webkitPerspective'] !== undefined || document.body.style['MozPerspective'] !== undefined){
-    var nodes = document.querySelectorAll(selector);
-    for(var i=0, len=nodes.length; i<len; i++){
-        var node = nodes[i];
-        node.innerHTML = '<span data-title="'+ node.text +'">' + node.innerHTML + '</span>';
-    };
-  }
-}
--- crisis/crisis/views.py
+++ crisis/crisis/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.template import RequestContext, loader
 
 def my404(request):
 	return render(request, 'crisis/404.html')
