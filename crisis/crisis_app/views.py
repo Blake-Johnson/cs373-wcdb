72fc043aa4ad8b407f8fc75440a7276f393c5967
stephenc <stephenchiang@utexas.edu>
2013-07-07 16:55:38 -0700

    updated contents page

--- crisis/crisis_app/views.py
+++ crisis/crisis_app/views.py
@@ -10,8 +10,8 @@ def about(request):
 	context = { 'author_list': author_list }
 	return render(request, 'crisis_app/about.html', context)
 
-def events(request, event_id=''):
-	if event_id == '':
+def events(request, event_id=None):
+	if event_id == None:
 		event_list = Event.objects.order_by('date_time')
 		image_list = []
 		for event in event_list:
@@ -19,22 +19,22 @@ def events(request, event_id=''):
 			if len(image) > 0:
 				image_list.append(image[0])
 			else:
-				image_list.append('')
+				image_list.append(None)
 		content_list = zip(event_list, image_list)
 		context = { 'content_list': content_list, 'type': 'Events', 'dir': 'events' }
 		return render(request, 'crisis_app/content.html', context)
 	else:
 		event = get_object_or_404(Event, id=event_id)
 		image = Embed.objects.filter(kind="IMG", event__id=event.id)
-		if len(image) == 0:
-			image = ''
-		else:
+		if len(image) > 0:
 			image = image[0]
-		context = { 'event': event, 'image': image }
+		people = Person.objects.filter(event__id=event.id)
+		orgs = Organization.objects.filter(event__id=event.id)
+		context = { 'event': event, 'image': image, 'people': people, 'orgs': orgs }
 		return render(request, 'crisis_app/event.html', context)
 
-def people(request, person_id=''):
-	if person_id == '':
+def people(request, person_id=None):
+	if person_id == None:
 		person_list = Person.objects.order_by('name')
 		image_list = []
 		for person in person_list:
@@ -42,24 +42,22 @@ def people(request, person_id=''):
 			if len(image) > 0:
 				image_list.append(image[0])
 			else:
-				image_list.append('')
+				image_list.append(None)
 		content_list = zip(person_list, image_list)
 		context = { 'content_list': content_list, 'type': 'People', 'dir': 'people' }
 		return render(request, 'crisis_app/content.html', context)
 	else:
 		person = get_object_or_404(Person, id=person_id)
 		image = Embed.objects.filter(kind="IMG", person__id=person.id)
-		if len(image) == 0:
-			image = ''
-		else:
+		if len(image) > 0:
 			image = image[0]
 		events = Event.objects.filter(person__id=person.id)
 		orgs = Organization.objects.filter(person__id=person.id)
 		context = { 'person': person, 'image': image, 'events': events, 'orgs': orgs }
 		return render(request, 'crisis_app/person.html', context)
 
-def orgs(request, org_id=''):
-	if org_id == '':
+def orgs(request, org_id=None):
+	if org_id == None:
 		org_list = Organization.objects.order_by('name')
 		image_list = []
 		for org in org_list:
@@ -67,16 +65,14 @@ def orgs(request, org_id=''):
 			if len(image) > 0:
 				image_list.append(image[0])
 			else:
-				image_list.append('')
+				image_list.append(None)
 		content_list = zip(org_list, image_list)
 		context = { 'content_list': content_list, 'type': 'Organizations', 'dir': 'orgs' }
 		return render(request, 'crisis_app/content.html', context)
 	else:
 		org = get_object_or_404(Organization, id=org_id)
 		image = Embed.objects.filter(kind="IMG", organization__id=org.id)
-		if len(image) == 0:
-			image = ''
-		else:
+		if len(image) > 0:
 			image = image[0]
 		events = Event.objects.filter(organization__id=org.id)
 		people = Person.objects.filter(organization__id=org.id)
