4d4e23ba8ffa98707bf6eab7fa7df29ee59cdd3f
stephenc <stephenchiang@utexas.edu>
2013-07-09 16:48:51 -0700

    documented crisis/views.py

--- crisis/crisis/views.py
+++ crisis/crisis/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def my404(request):
+	'''
+	Renders the 404 page when the user attempts to access
+		information or data that does not exist
+	Called from crisis/urls.py
+	'''
 	return render(request, 'crisis/404.html')
 
 def my500(request):
+	'''
+	Renders the 500 page when an internal server error occurs
+	Called from crisis/urls.py
+	'''
 	return render(request, 'crisis/500.html')
