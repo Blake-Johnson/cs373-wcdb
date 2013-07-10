f5990bf7fac5030cd2bb781163eaaf7094f7464e
stephenc <stephenchiang@utexas.edu>
2013-07-10 12:38:10 -0700

    updated urls documentation

--- crisis/crisis/urls.py
+++ crisis/crisis/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Since crisis is the main site, there is no redirect to an app's urls.py
+    # crisis_app takes care of components related to the site
     url(r'^$', 'crisis_app.views.index', name='index'),
     url(r'^(?i)about$', 'crisis_app.views.about', name='about'),
     url(r'^(?i)events/(?P<event_id>\d+)$', 'crisis_app.views.events', name='event'),
@@ -14,8 +14,11 @@ urlpatterns = patterns('',
     url(r'^(?i)events$', 'crisis_app.views.events', name='events'),
     url(r'^(?i)people$', 'crisis_app.views.people', name='people'),
     url(r'^(?i)orgs$', 'crisis_app.views.orgs', name='orgs'),
+
+    # crisis takes care of components beyond the site
     url(r'^(?i)404$', 'crisis.views.my404', name='404'),
     url(r'^(?i)500$', 'crisis.views.my500', name='500'),
+
     # Examples:
     # url(r'^$', 'crisis.views.home', name='home'),
     # url(r'^crisis/', include('crisis.foo.urls')),
