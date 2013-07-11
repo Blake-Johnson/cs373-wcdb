8135224c16854b04f168da768959fc0fe123139a
Stephen Chiang <stephenchiang@utexas.edu>
2013-07-11 10:04:48 -0500

    added spaces after commas in test

--- crisis/crisis_app/tests.py
+++ crisis/crisis_app/tests.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import datetime,json
+import datetime, json
 from os.path import join
 from subprocess import call, Popen, PIPE
 
@@ -20,48 +20,48 @@ XML = dict((f.split('.')[0], open(join(XML_FIXTURE_PATH, f)).read().strip())
 class ToJsonTestCase(TestCase):
 
 	def test_makeParent_1(self):
-		p = makeParent("test","color")
-		self.assertEqual(p['id'],"test")
-		self.assertEqual(p['data']['$color'],"color")
+		p = makeParent("test", "color")
+		self.assertEqual(p['id'], "test")
+		self.assertEqual(p['data']['$color'], "color")
 
 	def test_makeParent_2(self):
-		p = makeParent(1,"color")
-		self.assertEqual(p['id'],1)
-		self.assertEqual(p['data']['$color'],"color")
+		p = makeParent(1, "color")
+		self.assertEqual(p['id'], 1)
+		self.assertEqual(p['data']['$color'], "color")
 		
 	def test_makeParent_3(self):
-		p = makeParent((1,2),[1,2,3])
-		self.assertEqual(p['id'],(1,2))
-		self.assertEqual(p['data']['$color'],[1,2,3])		
+		p = makeParent((1, 2), [1, 2, 3])
+		self.assertEqual(p['id'], (1, 2))
+		self.assertEqual(p['data']['$color'], [1, 2, 3])		
 
 	def test_addChild_1(self):
-		p = makeParent("test","color")
-		addChild(p,'child','color',0,[],'desc')
-		self.assertEqual(p['children'][0]['id'],'child')
-		self.assertEqual(p['children'][0]['data']['$color'],'color')
-		self.assertEqual(p['children'][0]['data']['$area'],0)
-		self.assertEqual(p['children'][0]['data']['popularity'],0)
-		self.assertEqual(p['children'][0]['data']['image'],'None')
-		self.assertEqual(p['children'][0]['data']['description'],'desc')
+		p = makeParent("test", "color")
+		addChild(p, 'child', 'color', 0, [], 'desc')
+		self.assertEqual(p['children'][0]['id'], 'child')
+		self.assertEqual(p['children'][0]['data']['$color'], 'color')
+		self.assertEqual(p['children'][0]['data']['$area'], 0)
+		self.assertEqual(p['children'][0]['data']['popularity'], 0)
+		self.assertEqual(p['children'][0]['data']['image'], 'None')
+		self.assertEqual(p['children'][0]['data']['description'], 'desc')
 
 	def test_addChild_2(self):
-		p = makeParent("test","color")
-		addChild(p,'child','color',0,[],'desc')
-		addChild(p,'child','color',0,[1,2,3],'desc')
-		self.assertEqual(p['children'][1]['id'],'child')
-		self.assertEqual(p['children'][1]['data']['$color'],'color')
-		self.assertEqual(p['children'][1]['data']['$area'],0)
-		self.assertEqual(p['children'][1]['data']['popularity'],0)
-		self.assertEqual(p['children'][1]['data']['image'],'1')
-		self.assertEqual(p['children'][1]['data']['description'],'desc')
+		p = makeParent("test", "color")
+		addChild(p, 'child', 'color', 0, [], 'desc')
+		addChild(p, 'child', 'color', 0, [1, 2, 3], 'desc')
+		self.assertEqual(p['children'][1]['id'], 'child')
+		self.assertEqual(p['children'][1]['data']['$color'], 'color')
+		self.assertEqual(p['children'][1]['data']['$area'], 0)
+		self.assertEqual(p['children'][1]['data']['popularity'], 0)
+		self.assertEqual(p['children'][1]['data']['image'], '1')
+		self.assertEqual(p['children'][1]['data']['description'], 'desc')
 
 	def test_makeJSON_1(self):
 		j = makeJSON(3)
 		p = json.loads(j)
-		self.assertEqual(p["id"],"Crisis")
-		self.assertEqual(p['children'][0]['id'],"Events")
-		self.assertEqual(p['children'][1]['id'],"People")
-		self.assertEqual(p['children'][2]['id'],"Organizations")
+		self.assertEqual(p["id"], "Crisis")
+		self.assertEqual(p['children'][0]['id'], "Events")
+		self.assertEqual(p['children'][1]['id'], "People")
+		self.assertEqual(p['children'][2]['id'], "Organizations")
 
 class ToXmlTestCase(TestCase):
 
@@ -79,4 +79,3 @@ class ToXmlTestCase(TestCase):
 		except:
 		   pass
 		xml.kill()
-
