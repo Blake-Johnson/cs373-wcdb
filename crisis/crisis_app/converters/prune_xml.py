fa91cb70e13c810e6e93221d68686aaa9b5fd3b0
Aaron Stacy <aaron.r.stacy@gmail.com>
2013-07-11 16:04:22 -0500

    remove id's that we're not using

--- crisis/crisis_app/converters/prune_xml.py
+++ crisis/crisis_app/converters/prune_xml.py
@@ -20,6 +20,18 @@ def prune(xml):
 	new_tree = Element('WorldCrises')
 	for el in fromstring(xml):
 		if el.get('ID').split('_')[-1] in OUR_IDS:
-			new_tree.append(el)
+			new_el = fromstring(tostring(el))
+			remove_top_level = []
+			for child in new_el:
+				if child.tag in ['Crises', 'People', 'Organizations']:
+					to_remove = []
+					for link in child:
+						if link.get('ID')[4:] not in OUR_IDS:
+							to_remove.append(link)
+					[child.remove(l) for l in to_remove]
+					if not len(child):
+						remove_top_level.append(child)
+			[new_el.remove(c) for c in remove_top_level]
+			new_tree.append(new_el)
 	jacked =  minidom.parseString(tostring(new_tree)).toprettyxml()
 	return '\n'.join(l for l in jacked.split('\n') if l.strip())
