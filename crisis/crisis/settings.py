b5243cc8b8c3bcaeebcd83717c706dd020b6ca2a
Aaron Stacy <aaron.r.stacy@gmail.com>
2013-07-11 17:02:46 -0500

    use my db for production

--- crisis/crisis/settings.py
+++ crisis/crisis/settings.py
@@ -23,11 +23,11 @@ DATABASES = {
     'default': {
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.db.backends.' + ENGINE,
-        'NAME': 'crisis',                      # Or path to database file if using sqlite3.
+        'NAME': 'cs373_astacy',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'crisisuser',
-        'PASSWORD': 'crisispass',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'astacy',
+        'PASSWORD': '5ZSeqwT84W',
+        'HOST': 'z',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
