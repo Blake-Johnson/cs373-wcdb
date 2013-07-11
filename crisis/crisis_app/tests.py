5bb0a169c0a3560721cd86f30f681f5981978da0
David C <david.coon@utexas.edu>
2013-07-11 00:52:50 -0500

    added new tests... more to be added tomorrow. added some code w/ help from Aaron to add another check to killing the test DB

--- crisis/crisis_app/tests.py
+++ crisis/crisis_app/tests.py
@@ -1,7 +1,11 @@
 import sys
 import os
+import datetime,json
 from os.path import join
 from subprocess import call, Popen, PIPE
+
+from crisis_app import views
+from views import makeParent, addChild, makeJSON
 
 from django.test import TestCase
 
@@ -12,6 +16,52 @@ XML_FIXTURE_PATH = 'crisis_app/fixtures/xml'
 XML = dict((f.split('.')[0], open(join(XML_FIXTURE_PATH, f)).read().strip())
 	for f in os.listdir(XML_FIXTURE_PATH))
 
+
+class ToJsonTestCase(TestCase):
+
+	def test_makeParent_1(self):
+		p = makeParent("test","color")
+		self.assertEqual(p['id'],"test")
+		self.assertEqual(p['data']['$color'],"color")
+
+	def test_makeParent_2(self):
+		p = makeParent(1,"color")
+		self.assertEqual(p['id'],1)
+		self.assertEqual(p['data']['$color'],"color")
+		
+	def test_makeParent_3(self):
+		p = makeParent((1,2),[1,2,3])
+		self.assertEqual(p['id'],(1,2))
+		self.assertEqual(p['data']['$color'],[1,2,3])		
+
+	def test_addChild_1(self):
+		p = makeParent("test","color")
+		addChild(p,'child','color',0,[],'desc')
+		self.assertEqual(p['children'][0]['id'],'child')
+		self.assertEqual(p['children'][0]['data']['$color'],'color')
+		self.assertEqual(p['children'][0]['data']['$area'],0)
+		self.assertEqual(p['children'][0]['data']['popularity'],0)
+		self.assertEqual(p['children'][0]['data']['image'],'None')
+		self.assertEqual(p['children'][0]['data']['description'],'desc')
+
+	def test_addChild_2(self):
+		p = makeParent("test","color")
+		addChild(p,'child','color',0,[],'desc')
+		addChild(p,'child','color',0,[1,2,3],'desc')
+		self.assertEqual(p['children'][1]['id'],'child')
+		self.assertEqual(p['children'][1]['data']['$color'],'color')
+		self.assertEqual(p['children'][1]['data']['$area'],0)
+		self.assertEqual(p['children'][1]['data']['popularity'],0)
+		self.assertEqual(p['children'][1]['data']['image'],'1')
+		self.assertEqual(p['children'][1]['data']['description'],'desc')
+
+	def test_makeJSON_1(self):
+		j = makeJSON(3)
+		p = json.loads(j)
+		self.assertEqual(p["id"],"Crisis")
+		self.assertEqual(p['children'][0]['id'],"Events")
+		self.assertEqual(p['children'][1]['id'],"People")
+		self.assertEqual(p['children'][2]['id'],"Organizations")
 
 class ToXmlTestCase(TestCase):
 
@@ -24,5 +74,9 @@ class ToXmlTestCase(TestCase):
 		devnull = open(os.devnull, 'w')
 		self.assertEqual(call('./manage.py validate'.split(),
 			stdin=xml.stdout, stdout=devnull, stderr=devnull), 0)
-		devnull.close()
+		try:
+   			devnull.close()
+		except:
+		   pass
 		xml.kill()
+
