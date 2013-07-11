986abfcf9c557f94ba7ce46c5fcffaa767e91af1
stephenc <stephenchiang@utexas.edu>
2013-07-10 20:48:51 -0700

    url to XML is /xml

--- crisis/crisis/urls.py
+++ crisis/crisis/urls.py
@@ -14,7 +14,6 @@ urlpatterns = patterns('',
     url(r'^(?i)events$', 'crisis_app.views.events', name='events'),
     url(r'^(?i)people$', 'crisis_app.views.people', name='people'),
     url(r'^(?i)orgs$', 'crisis_app.views.orgs', name='orgs'),
-    url(r'^(?i)xml$', 'crisis_app.views.xml', name='xml'),
 
     # crisis takes care of components beyond the site
     url(r'^(?i)404$', 'crisis.views.my404', name='404'),
@@ -30,7 +29,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^WorldCrises.xml', 'crisis_app.views.xml'),
+    url(r'^(?i)xml$', 'crisis_app.views.xml', name='xml'),
 )
 
 # Directs server error codes to proper views
