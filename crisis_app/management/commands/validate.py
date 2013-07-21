8e0fb07fc76853a9b3cc2d2626adb29c0bdb4329
stephenc <stephenchiang@utexas.edu>
2013-07-21 13:11:37 -0700

    fixed xml issue

--- crisis_app/management/commands/validate.py
+++ crisis_app/management/commands/validate.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand, CommandError
 from minixsv.pyxsval import parseAndValidateXmlInputString
 
 XSD = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-	'../../../assets/xml/astacy-WCDB1.xsd.xml')).read()
+	'../../fixtures/xml/wcdb_schema.xsd.xml')).read()
 
 class Command(BaseCommand):
 	help = 'validates xml input'
