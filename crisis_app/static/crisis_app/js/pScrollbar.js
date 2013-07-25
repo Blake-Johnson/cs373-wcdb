46fca12b525e8002514c947ce674475d2c98b9f5
Stephen Chiang <stephenchiang@utexas.edu>
2013-07-25 17:48:32 -0500

    maybe this will close 111

--- crisis_app/static/crisis_app/js/pScrollbar.js
+++ crisis_app/static/crisis_app/js/pScrollbar.js
@@ -1,4 +1,16 @@
-/* jQuery Mousewheel */
+/* Credits here: https://github.com/noraesae/perfect-scrollbar */
+/* jQuery Mousewheel, credits here: https://github.com/brandonaaron/jquery-mousewheel */
+/*! Copyright (c) 2011 Brandon Aaron (http://brandonaaron.net)
+ * Licensed under the MIT License (LICENSE.txt).
+ *
+ * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
+ * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
+ * Thanks to: Seamus Leahy for adding deltaX and deltaY
+ *
+ * Version: 3.0.6
+ * 
+ * Requires: 1.2.2+
+ */
 (function($) {
 
 var types = ['DOMMouseScroll', 'mousewheel'];
