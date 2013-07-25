97f522cb12ddef0a9bc3a1643f91f7ca9dd69d63
Blake-Johnson <blakejohnson92@utexas.edu>
2013-07-25 17:30:04 -0500

    Added regex test suite

--- crisis_app/tests.py
+++ crisis_app/tests.py
@@ -159,6 +159,73 @@ class ImportExportTest(UsefulTestCase):
 			# write the wrong file out to disk for inspection from the cmd line
 			open('invalid_response.xml', 'w').write(response.content)
 			raise e
+			
+class RegexTestCase(UsefulTestCase):
+	def test_youtube_working_embed(self):
+		testString_1 = youtube_to_embed("http://www.youtube.com/watch?v=RUKsyA_z7n8","youtube_1")
+		testString_2 = youtube_to_embed("http://www.youtube.com/watch?v=nBzHCVv5GeQ","youtube_2")
+		testString_3 = youtube_to_embed("http://www.youtube.com/watch?v=5gg1hkd5wnI","youtube_3")
+
+		assert testString_1 == '<iframe width="560" height="315" src="http://www.youtube.com/embed/RUKsyA_z7n8" frameborder="0" name="youtube_1?wmode=opaque" allowfullscreen></iframe>'
+
+		assert testString_2 == '<iframe width="560" height="315" src="http://www.youtube.com/embed/nBzHCVv5GeQ" frameborder="0" name="youtube_2?wmode=opaque" allowfullscreen></iframe>'
+
+		assert testString_3 == '<iframe width="560" height="315" src="http://www.youtube.com/embed/5gg1hkd5wnI" frameborder="0" name="youtube_3?wmode=opaque" allowfullscreen></iframe>'
+
+	def test_youtube_semiWorking_embed(self):
+		testString_1 = youtube_to_embed("http://vimeo.com/13595568","youtube_1")
+		testString_2 = youtube_to_embed("http://vimeo.com/1532967","youtube_2")
+		testString_3 = youtube_to_embed("http://www.youtube.com/vatican","youtube_3")
+
+		assert testString_1 == '<a href="http://vimeo.com/13595568">youtube_1 (vimeo)</a><br />'
+
+		assert testString_2 == '<a href="http://vimeo.com/1532967">youtube_2 (vimeo)</a><br />'
+
+		assert testString_3 == '<a href="http://www.youtube.com/vatican">youtube_3 (youtube)</a><br />'
+
+	def test_youtube_notWorking_embed(self):
+		testString_1 = youtube_to_embed("http://youtu.be/_iNBKlBcXkY","youtube_1")
+		testString_2 = youtube_to_embed("http://commons.wikimedia.org/wiki/Obama_Administration_weekly_video_addresses","youtube_2")
+		testString_3 = youtube_to_embed("http://www.tepco.co.jp/en/news/library/movie-01e.html","youtube_3")
+
+		assert testString_1 == '<a href="http://youtu.be/_iNBKlBcXkY">youtube_1</a><br />'
+
+		assert testString_2 == '<a href="http://commons.wikimedia.org/wiki/Obama_Administration_weekly_video_addresses">youtube_2</a><br />'
+
+		assert testString_3 == '<a href="http://www.tepco.co.jp/en/news/library/movie-01e.html">youtube_3</a><br />'
+
+	def test_markurls(self):
+		testString_1 = mark_urls('tapkorea@gmail.com')
+		testString_2 = mark_urls('http://www.valvesoftware.com/')
+		testString_3 = mark_urls('URL: http://www.dfps.state.tx.us/child_protection/ address: 701 W. 51st St. Austin, Texas 78751 Phone: (512) 438-4800')
+		testString_4 = mark_urls('FLDS.org')
+		testString_5 = mark_urls('Phone: 1-800-FOR-KIDS')
+		testString_6 = mark_urls('Phone: 720-898-5900 URL: http://www.communityfirstfoundation.org/index.cfm Address: 6870 West 52nd Avenue, Suite 103 Arvada, Colorado 80002')
+		testString_7 = mark_urls('The Salvation Army National Headquarters 615 Slaters Lane P.O. Box 269 Alexandria, VA 22313')
+		testString_8 = mark_urls('http://www.oref.org.il/International/14-en/PAKAR.aspx')
+		testString_9 = mark_urls('(405) 235-3313')
+		testString_10 = mark_urls('http://www.redcross.org/contact-us/general-inquiry')
+
+		assert testString_1 == '<a href="mailto:tapkorea@gmail.com" target="_blank">tapkorea@gmail.com<span></span></a>'
+
+		assert testString_2 == '<a href="http://www.valvesoftware.com/" target="_blank">http://www.valvesoftware.com/<span></span></a>'
+
+		assert testString_3 == 'URL: <a href="http://www.dfps.state.tx.us/child_protection/" target="_blank">http://www.dfps.state.tx.us/child_protection/<span></span></a> address: 701 W. 51st St. Austin, Texas 78751 Phone: (512) 438-4800'
+
+		assert testString_4 == '<a href="http://FLDS.org" target="_blank">FLDS.org<span></span></a>'
+
+		assert testString_5 == 'Phone: 1-800-FOR-KIDS'
+
+		assert testString_6 == 'Phone: 720-898-5900 URL: <a href="http://www.communityfirstfoundation.org/index.cfm" target="_blank">http://www.communityfirstfoundation.org/index.cfm<span></span></a> Address: 6870 West 52nd Avenue, Suite 103 Arvada, Colorado 80002'
+
+		assert testString_7 == 'The Salvation Army National Headquarters 615 Slaters Lane P.O. Box 269 Alexandria, VA 22313'
+
+		assert testString_8 == '<a href="http://www.oref.org.il/International/14-en/PAKAR.aspx" target="_blank">http://www.oref.org.il/International/14-en/PAKAR.aspx<span></span></a>'
+
+		assert testString_9 == '(405) 235-3313'
+
+		assert testString_10 == '<a href="http://www.redcross.org/contact-us/general-inquiry" target="_blank">http://www.redcross.org/contact-us/general-inquiry<span></span></a>'
+
 
 class ParseQuerifyTest(TestCase):
 	fixtures = ['test_data.json']
