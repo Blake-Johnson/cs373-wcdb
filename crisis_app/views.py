11f9b8134099d47d089dd9968bafdb6998af8393
stephenc <stephenchiang@utexas.edu>
2013-07-27 19:08:25 -0700

    closes #125

--- crisis_app/views.py
+++ crisis_app/views.py
@@ -255,7 +255,7 @@ def youtube_to_embed(url, name):
 		embed, it attempts to provide information about the video's
 		source
 	'''
-	match = re.search(r'^(?:http|https)\:\/\/www\.youtube\.com\/watch\?(?:feature\=[a-z_]*&)?v\=([\w\-]*)(?:\&(?:.*))?$', url)
+	match = re.search(r'^(?:http|https)\:\/\/www\.youtube\.com\/(?:watch\?(?:feature\=[a-z_]*&)?v\=|embed\/)([\w\-]*)(?:\&(?:.*))?$', url)
 	if match:
 		embed_url = 'http://www.youtube.com/embed/%s' %(match.group(1))
 		res = '<iframe width="560" height="315" src="%s" frameborder="0" name="%s?wmode=opaque" allowfullscreen></iframe>' %(embed_url, name)
